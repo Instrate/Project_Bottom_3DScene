e84b8d971bd2cfec9aca3cc7ac087af92d3ab645
Instrate <Instrate@users.noreply.github.com>
2021-04-24 00:40:01 +0300

    -added calculation formulae for finding angles and coefs to get the best z-value when intersect happens

--- Scene/Scene.cs
+++ Scene/Scene.cs
@@ -51,7 +51,7 @@ namespace Scene
             _shader = new Shader(PATH.SHADER_VERT, PATH.SHADER_FRAG);
             _shader.Use();
             _shader.SetInt("texture0", 0);
-            
+
             _landscape = new World(20, 20, -1, _shader);
             _landscape.loadTextureFromFile(PATH.TEXTURE_PLANE);
 
@@ -117,7 +117,7 @@ namespace Scene
 
         public void OnKeyDown(PreviewKeyDownEventArgs e)
         {
-            
+
 
             double time = _clock.Elapsed.TotalMilliseconds / 1000.0;
             _clock.Restart();
@@ -183,15 +183,15 @@ namespace Scene
             int points = 4;
             int offset = size / points;
             float[][] matr = new float[points][];
-            for(short i = 0; i < points; i++)
+            for (short i = 0; i < points; i++)
             {
                 matr[i] = new float[3];
-                for(short j = 0; j < 3; j++)
+                for (short j = 0; j < 3; j++)
                 {
                     matr[i][j] = 0;
                 }
             }
-            for(int ii = 0, i = 0, j = 0; i < points; ii -= 3, ii = ii + offset, i++, j = 0)
+            for (int ii = 0, i = 0, j = 0; i < points; ii -= 3, ii = ii + offset, i++, j = 0)
             {
                 matr[i][j] = vert[ii];
                 ii++; j++;
@@ -205,29 +205,29 @@ namespace Scene
         public float[][] doFindMinMaxVector(float[][] matr)
         {
             float[][] vert = new float[2][];
-            for(short i = 0; i < 2; i++)
+            for (short i = 0; i < 2; i++)
             {
                 vert[i] = new float[3];
-                for(short j = 0; j < 3; j++)
+                for (short j = 0; j < 3; j++)
                 {
                     vert[i][j] = matr[i][j];
                 }
             }
-                
-            for(short i = 0; i < 4; i++)
+
+            for (short i = 0; i < 4; i++)
             {
-                for(short j = 0; j < 3; j++)
+                for (short j = 0; j < 3; j++)
                 {
-                    if(vert[0][j] > matr[i][j])
+                    if (vert[0][j] > matr[i][j])
                     {
                         vert[0][j] = matr[i][j];
-                    } 
+                    }
                     if (vert[1][j] < matr[i][j])
                     {
-                        vert[1][j] = matr[i][j]; 
+                        vert[1][j] = matr[i][j];
                     }
-                    
-                    
+
+
                 }
             }
             return vert;
@@ -241,7 +241,7 @@ namespace Scene
             uint cols = _landscape.bottom.cols;
             uint size = rows * cols;
             float[][][] btm = new float[size][][];
-            for(uint i = 0, k = 0; i < cols; i++)
+            for (uint i = 0, k = 0; i < cols; i++)
             {
                 for (uint j = 0; j < rows; j++)
                 {
@@ -253,15 +253,33 @@ namespace Scene
             float[][] flsect = new float[size][];
             for (uint i = 0, j = 0; i < size; i++)
             {
-                if(sect[1][0] >= btm[i][0][0] && sect[1][1] >= btm[i][0][1])
+                if (sect[1][0] >= btm[i][0][0] && sect[1][1] >= btm[i][0][1])
                 {
+                    float x1 = sect[0][0];
+                    float x2 = sect[1][0];
+                    float x3 = btm[i][0][0];
+                    float x4 = btm[i][1][0];
+                    float y1 = sect[0][1];
+                    float y2 = sect[1][1];
+                    float y3 = btm[i][0][1];
+                    float y4 = btm[i][1][1];
+                    float p = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
                     flsect[j] = new float[3];
-                    
+                    float x0 = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / p;
+                    float y0 = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / p;
+
+                    float k10 = (y1 - y0) / (x1 - x0);
+                    float k20 = (y3 - y0) / (x3 - x0);
+                    float tgAngle = Math.Abs((k10 - k20) / (1 + k10 * k20));
+                    float X1 = Math.Abs(x3 - x0) * tgAngle;
                     //calculation stuff
+                    flsect[j][0] = X1;
+                    flsect[j][1] = 0;
+                    flsect[j][2] = 0;
                     j++;
                 }
             }
-              
+
         }
     }
 }
